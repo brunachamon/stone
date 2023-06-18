ab943afe0320fb970b89d5c75f15d1fd6fd141f8
Bruna Chamon <bruchamon@gmail.com>
2023-06-18 17:34:07 -0500

    Increasing tests

--- app/reduxConfigTests.jsx
+++ app/reduxConfigTests.jsx
@@ -2,28 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { render as rtlRender } from "@testing-library/react";
 import { reducers } from "./src/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// import store from "./src/store";
-
-// const render = (ui) => {
-//   // const store = configureStore({ reducer: reducers });
-
-//   const Wrapper = ({ children }) => (
-//     <Provider store={store}>{children}</Provider>
-//   );
-
-//   return rtlRender(ui, { wrapper: Wrapper });
-// };
-
-// export default render;
-
-const store = ({ preloadedState } = {}) => configureStore({ reducer: reducers, preloadedState });
+const store = ({ preloadedState } = {}) =>
+  configureStore({ reducer: reducers, preloadedState });
 
 function render(ui, { initialState, ...renderOptions } = {}) {
   function Wrapper({ children }) {
     return (
       <Provider store={store({ preloadedState: initialState })}>
-        {children}
+        <Router>{children}</Router>
       </Provider>
     );
   }
---
+++ app/src/pages/Login.test.jsx
@@ -0,0 +1,74 @@
+import { fireEvent } from "@testing-library/react";
+import Login from "./Login";
+
+import render from "../../reduxConfigTests";
+
+const login = {
+  email: "bruna@teste.com",
+  password: "senhadeteste",
+};
+
+const mockHandleLoginFn = jest.fn();
+const mockDispatchFn = jest.fn();
+
+beforeAll(() => {
+  jest.mock("../slices/user", () => ({
+    handleLogin: mockHandleLoginFn,
+  }));
+});
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatchFn,
+}));
+
+test("should reflect email and password values changing when triggering events on input", () => {
+  const { getByLabelText } = render(<Login />);
+
+  const emailInput = getByLabelText("E-mail");
+  const passwordInput = getByLabelText("Senha");
+
+  fireEvent.change(emailInput, { target: { value: login.email } });
+  fireEvent.change(passwordInput, { target: { value: login.password } });
+
+  expect(emailInput.value).toBe(login.email);
+  expect(passwordInput.value).toBe(login.password);
+});
+
+test("should submit the form values and navigate when is successfull", async () => {
+  const { getByText, getByLabelText } = render(<Login />);
+
+  const emailInput = getByLabelText("E-mail");
+  const passwordInput = getByLabelText("Senha");
+  const submitButton = getByText("Entrar");
+
+  fireEvent.change(emailInput, { target: { value: login.email } });
+  fireEvent.change(passwordInput, { target: { value: login.password } });
+
+  fireEvent.click(submitButton);
+
+  await expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+});
+
+test("should simulate an error in login", async () => {
+  const error = new Error("Login falhoooou");
+
+  const mockHandleLoginFn = jest.fn(() => Promise.reject(error));
+
+  jest.mock("../slices/user", () => ({
+    handleLogin: mockHandleLoginFn,
+  }));
+
+  const { getByText, getByLabelText } = render(<Login />);
+
+  const emailInput = getByLabelText("E-mail");
+  const passwordInput = getByLabelText("Senha");
+  const submitButton = getByText("Entrar");
+
+  fireEvent.change(emailInput, { target: { value: login.email } });
+  fireEvent.change(passwordInput, { target: { value: login.password } });
+
+  fireEvent.click(submitButton);
+
+  await expect(mockDispatchFn).toHaveBeenCalledTimes(1);
+});
